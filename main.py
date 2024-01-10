c8344f3c798abe0969e770275b07df40bb06c692
Pinukio <gkswjddnjs15@gmail.com>
2024-01-10 22:11:00 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# 바닥부터 배우는 강화 학습 P.184 Deep RL을 이용한 Cosine Function Fitting 구현
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import numpy as np
+import matplotlib.pyplot as plt
+
